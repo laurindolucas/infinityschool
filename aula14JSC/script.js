07d58cbb4f0f2a6373ae2195d38808513da665f2
Lucas Laurindo <caiolucax2024@gmail.com>
2025-06-07 10:25:35 -0300

    AULA 14 FINAL

--- aula14JSC/script.js
+++ aula14JSC/script.js
@@ -1,9 +1,21 @@
-fetch('https://jsonplaceholder.typicode.com/users') 
-    .then(response => response.json())
-    .then(users =>
-    {
-        for (const user of users) {
-            console.log(user.address)
-        }
-    })
-    .catch(error => console.error("Erro: " ))
+// fetch('https://jsonplaceholder.typicode.com/users') 
+//     .then(response => response.json())
+//     .then(users =>
+//     {
+//         for (const user of users) {
+//             console.log(user.address)
+//         }
+//     })
+//     .catch(error => console.error("Erro: " ))
+const cep = document.getElementById("cep");
+
+const button = document.getElementById('btnCep');
+button.addEventListener('click', async function consultarCep() {
+    try {
+        const consulta = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+        const resposta = await consulta.json();
+        console.log(resposta);
+    }catch (erro){
+        console.error('Erro:', erro);
+    } 
+})
